b54832a012c3cc4ad37168af5574bd6482623ba4
Monica <monica.montero@tyris-software.com>
2022-10-26 11:52:40 +0200

    update main.py: change order view in left column

--- main.py
+++ main.py
@@ -65,15 +65,15 @@ if __name__=='__main__':
 
             print(vid)
 
+            title = st.sidebar.text_input('Movie title', 'Type movie title here...')
+            st.sidebar.write('The current movie title is: ', title)
+
+            description = st.sidebar.text_input('Movie description', 'Type movie description here...')
+            st.sidebar.write('The current movie description is: ', description)
+
             if( os.path.splitext(vid)[0] == '6863'):
-                print('ok')
-            
-                title = st.sidebar.text_input('Movie title', 'Men in Black')
-                st.sidebar.write('The current movie title is: ', title)
-
-                description = st.sidebar.text_input('Movie description', 'After a police chase with an otherworldly being, a New York City cop is recruited as an agent in a top-secret organization established to monitor and police alien activity on Earth: the Men in Black. Agent Kay and new recruit Agent Jay find themselves in the middle of a deadly plot by an intergalactic terrorist who has arrived on Earth to assassinate two ambassadors from opposing galaxies.')
-                st.sidebar.write('The current movie description is: ', description)
-
+                print('ok')         
+                
                 bytes_data = uploaded_file.getvalue()
                 st.video(bytes_data, format="video/mp4", start_time=0)
 
